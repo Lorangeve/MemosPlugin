55807d794c0c13c9fa5b0547f638ae29b0c778aa
L🍊Vu <44472184+Lorangeve@users.noreply.github.com>
2024-10-25 18:15:02 +0800

    :apple: 闭包

--- tag_replace.js
+++ tag_replace.js
@@ -1,16 +1,13 @@
-console.log("加载中...");
-
-['https://code.jquery.com/jquery-3.7.1.min.js'].forEach((s) => {
-    const script = document.createElement('script');
-    script.src = s;
-    document.head.appendChild(script);
-})
-
-window.onload = () => {
-    // 这里的代码会在整个页面（包括图片）加载完成后执行
-    console.log("页面完全加载完成");
-    jQuery && console.log("注入 jQuery 成功！");
-    const $ = jQuery;
-
-    $("p").text("替换成功")
-};
+(function (params) {
+    ['https://code.jquery.com/jquery-3.7.1.min.js'].forEach((s) => {
+        const script = document.createElement('script');
+        script.src = s;
+        document.head.appendChild(script);
+    })
+    window.onload = () => {
+        // 这里的代码会在整个页面（包括图片）加载完成后执行
+        console.log("页面完全加载完成");
+        jQuery && console.log("注入 jQuery 成功！");
+        const $ = jQuery;
+    };
+})()
